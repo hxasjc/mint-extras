f74a06738ebad6de374c552e80e3344fd6c47e44
chaslinux <chaslinux@gmail.com>
2024-06-14 06:27:45 -0400

    Changed most applications to debian packages.

--- mint-extras.sh
+++ mint-extras.sh
@@ -18,7 +18,6 @@ sudo apt install git build-essential curl -y
 # Install btop
 sudo apt install btop -y
 
-
 # Install phoronix-test-suite for testing, and for extensive system information
 # make a directory to put github projects in, then clone the
 # phoronix-test-suite repository to it
@@ -45,37 +44,39 @@ sudo apt install cpu-x hardinfo -y
 sudo flatpak install com.discordapp.Discord zoom -y
 
 # Install graphics software
-sudo flatpak install org.kde.krita org.kde.krita.Codecs -y
-sudo flatpak install org.gimp.GIMP org.gimp.GIMP.Manual org.gimp.GIMP.Plugin.GMic/x86_64/2-40 -y
-# Install some GIMP plugins
-sudo flatpak install org.gimp.GIMP.Plugin.Resynthesizer/x86_64/2-40 -y
-sudo flatpak install org.gabmus.swatch -y
+sudo apt install krita -y
+sudo apt install gimp gimp-gmic gimp-guntenprint gimp-help-en gimp-lensfun gimp-resynthesizer gimp-save-for-web -y
+# sudo flatpak install org.gabmus.swatch -y
 
 # Install webcam software, vector drawing software, and 3D graphics software
 sudo apt install guvcview cheese -y
 sudo apt install inkscape -y
-sudo flatpak install org.blender.Blender -y
+sudo apt install blender -y
 
 # The following are for managing photographs
-sudo flatpak install darktable -y
-sudo flatpak install rawtherapee -y
-sudo apt install rapid-photo-downloader -y
+sudo apt install darktable rawtherapee rapid-photo-downloader -y
 sudo flatpak install com.xnview.XnViewMP -y
 sudo flatpak install org.kde.digikam -y
-# sudo apt install showfoto -y ## KDE App so a lot of dependencies
-
 
 # Install multimedia software
-sudo apt install vlc smplayer -y
-sudo flatpak install com.spotify.Client -y
+sudo apt install vlc smplayer audacity xfburn mixxx -y
+sudo apt install spotify-client asunder picard -y
 
 # Install Game software
-sudo apt install steam -y
+sudo apt install steam burgerspace gnome-mines tuxtype frozen-bubble gewled -y
+sudo apt install anagramarama freedroid open-invaders aisleriot -y
 
 # Install software to manage documents
-sudo flatpak install org.onlyoffice.desktopeditors com.calibre_ebook.calibre -y
-sudo apt install qtqr -y
+sudo flatpak install org.onlyoffice.desktopeditors -y
+sudo flatpak install net.rpdev.OpenTodoList -y
+sudo apt install qtqr calibre -y
 
 # Install software development stuff
-sudo apt install godot3 -y
+sudo apt install godot3 sublime-text filezilla -y
+
+# Install Internet stuff
+sudo apt install chromium google-earth-pro-stable remmina -y
+
+# Now for a few system utilities
+sudo apt install gnome-disk-utility warpinator gparted memtest86+ gnome-system-monitor -y
 
