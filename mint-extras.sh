9869e39a1dd970c92df1446995428853936f50f5
hxasjc <92391383+hxasjc@users.noreply.github.com>
2025-07-28 21:40:09 -0400

    attempt to mass trust dekstop files

--- mint-extras.sh
+++ mint-extras.sh
@@ -214,9 +214,19 @@ xfconf-query -c xfce4-desktop -p /desktop-icons/file-icons/show-home --set true
 xfconf-query -c xfce4-desktop -p /desktop-icons/file-icons/show-removable --set true
 xfconf-query -c xfce4-desktop -p /desktop-icons/file-icons/show-trash --set true
 
+#function to mass trust .desktop files that we copy over
+function mass_trust_desktop {
+	for f in "$1/*.desktop"; do
+ 		chmod +x "$f"
+   		gio set -t string "$f" metadata::xfce-exe-checksum "$(sha256sum "$f" | awk '{print $1}')"
+     	done
+}
+
 #install shortcuts to current user desktop
 cp $currentdir/desktop/* ~/Desktop
+mass_trust_desktop ~/Desktop
 
 #install shortcuts to skel desktop
 sudo mkdir /etc/skel/Desktop
 sudo cp $currentdir/desktop/* /etc/skel/Desktop
+sudo mass_trust_desktop /etc/skel/Desktop
